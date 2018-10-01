804fd3142f761118a0b8dbe9948f87ad7b761b6e
Koltsov <koltsov.iv@gmail.com>
2018-10-01 22:05:16 +0300

    add summ digits func

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,4 +18,39 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+
+    public function test_sum_digits()
+    {
+        $arg1 = '9000000000000000000000000655';
+        $arg2 = '2000000000000000000000000712';
+        $result = $this->sumBigDigits($arg1, $arg2);
+        $this->assertEquals('11000000000000000000000001367', $result);
+    }
+
+    public function sumBigDigits(string $arg1, string $arg2): string
+    {
+        $max = $arg1 >= $arg2 ? $arg1 : $arg2;
+        $min = $arg1 == $max ? $arg2 : $arg1;
+        $stackMax = array_reverse(str_split($max));
+        $stackMin = array_reverse(str_split($min));
+        $resultStack = [];
+        $uppNextLevel = false;
+        foreach ($stackMax as $key => $value) {
+            $sum = $value + @$stackMin[$key] + $uppNextLevel;
+            $uppNextLevel = false;
+            if ($sum < 10) {
+                array_push($resultStack, (string)$sum);
+            } else {
+                $uppNextLevel = true;
+                array_push($resultStack, ((string)$sum)[1]);
+            }
+        }
+
+        if ($uppNextLevel) {
+            array_push($resultStack, '1');
+        }
+
+        return implode("", array_reverse($resultStack));
+    }
 }
